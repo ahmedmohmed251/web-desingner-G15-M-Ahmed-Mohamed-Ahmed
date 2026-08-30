b4a8f429059c97ffc5176e74e47eea0c14cac314
ahmedmohmed251 <ahmadmohmed251@gmail.com>
2026-08-30 13:01:40 +0300

    session 10-part 2

--- Session 10 -part 2/js/script.js
+++ Session 10 -part 2/js/script.js
@@ -1,16 +1,24 @@
 function looping(startNum, endNum, breakNum, contNum) {
-     if ( startNum === undefined || endNum === undefined || breakNum === undefined || contNum === undefined) {
+
+    if (
+        startNum === undefined ||
+        endNum === undefined ||
+        breakNum === undefined ||
+        contNum === undefined)
+        {
         console.log("Please enter your numbers");
-        return;
-    }
-    for (var i = startNum; i <= endNum; i++) {
+        return;}
+
+    for (let i = startNum; i <= endNum; i++) {
+
         if (i === breakNum) {
             break;
         }
+
         if (i === contNum) {
             continue;
         }
+
         console.log(i);
     }
 }
-looping(1, 10, 8, 3);
